e9c4d7d5652aa0de0787d20e8ea17da4a2e5d69a
Jorge Pereira <jorge.brito.xyz@gmail.com>
2022-10-19 19:24:49 -0300

    Helpful stuff

--- examples/utils.jl
+++ examples/utils.jl
@@ -11,8 +11,15 @@ end
 const THEN = Ref(time())
 const TIME = Ref(0.0)
 
+function printf(str, values...)
+    io = IOBuffer()
+    fmt = Printf.Format(str)
+    Printf.format(io, fmt, values...)
+    return String(take!(io))
+end
+
 Base.@kwdef mutable struct TimeInfo
-    then::Float64 = time()
+    then::Float64 = 1e-9 * time_ns()
     now::Float64 = 0.0
     elapsed::Float64 = 0.0
     frametime::Float64 = 0.0
@@ -20,7 +27,7 @@ Base.@kwdef mutable struct TimeInfo
 end
 
 function update!(info::TimeInfo)
-    info.now = time()
+    info.now = 1e-9 * time_ns()
     info.frametime = info.now - info.then
     info.elapsed += info.frametime
     info.framerate = 1 / info.frametime
